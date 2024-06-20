6b575dff7f4e2da2ae1d64a6401fef8d95da80f4
Hunter Kang <hunterkang5@g.ucla.edu>
2024-06-19 18:48:26 -0700

    Added routing between pages and added folders for the different pages

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Home.scss';
+
+function Home(): JSX.Element {
+    return ( 
+        <div>
+            <h1> HOME PAGE</h1>
+        </div>
+ );
+}
+
+export default Home;
