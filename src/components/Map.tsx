4372d9cd8b21540506f81f9160c070095ce25256
night-pianist <sweetkhiet907@gmail.com>
2024-06-21 20:20:37 -0700

    small changes

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
 import ReactMapGL from "react-map-gl";
+import '../styles/Map.css';
 
 const Map = () => {
     const [viewport, setViewport] = useState({
-        latitude: 34.0699,
-        longitude: 118.4441,
-        width: "100vw",
+        latitude: 34.0699, // ucla latitude and longitude as default
+        longitude: -118.4441,
+        width: "100vw", // fix map isnt showing full screen
         height: "100vh",
         zoom: 10
     });
+
+    const handleViewportChange = () => {
+        setViewport(viewport);
+      };    
 
     return ( 
         <div className="map">
             <ReactMapGL
               { ...viewport } 
               mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-              mapStyle = "mapbox://styles/mapbox/outdoors-v12"
-            //   onViewportChange={viewport => setViewport(viewport)}
+              mapStyle = "mapbox://styles/mapbox/dark-v11"
+            //   onViewportChange={handleViewportChange}
             >
 
             </ReactMapGL>
