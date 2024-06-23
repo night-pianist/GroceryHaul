a8a708334f68d2ae0e34ab4e448b06969da1803b
NotAmaia <012.sva@gmail.com>
2024-06-22 23:43:03 -0700

    added user authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-import Home from './components/Home'
+import Home from './components/Home';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import Signup from './components/Signup';
 
 function App() {
   return (
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
   )
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import '../styles/Home.scss';
 
 function Home(): JSX.Element {
     return ( 
         <div>
             <h1> HOME PAGE</h1>
+            <nav> 
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/signup">Signup</Link></li>
+                </ul>
+            </nav>
         </div>
  );
 }
