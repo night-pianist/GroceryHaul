9f55f74beb7ebf6a2ebbfd2f52bd61a05a70dae8
Hunter Kang <hunterkang5@g.ucla.edu>
2024-06-22 21:10:39 -0700

    Added animated map background should be responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import Home from './components/Home'
 
 function App() {
   return (
-      <Router>
+      <>
         <Routes>
           <Route path="/" element={<Home/>} />
         </Routes>
-      </Router>
+      </>
   )
 }
 
