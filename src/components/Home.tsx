ed9e068316db9c04821bafbc34be6e3395b7ecbd
Hunter Kang <hunterkang5@g.ucla.edu>
2024-06-24 13:28:16 -0700

    added logo, whitebox, and buttons

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,13 @@
 import React, {useState} from 'react'
 import '../styles/Home.css'
-import mapAnimation from 'public/MapArrowAnimation.gif'
-import staticMap from 'public/StaticBackgroundMap.svg'
+
 
 function Home(): JSX.Element {
     const mapAnimationPath = `${process.env.PUBLIC_URL}/MapArrowAnimation.gif`;
     const staticMapPath = `${process.env.PUBLIC_URL}/StaticBackgroundMap.svg`;
+    const whiteBox = `${process.env.PUBLIC_URL}/WhiteBox.png`;
+    const groceryLogo = `${process.env.PUBLIC_URL}/GroceryLogo.png`;
+
   
     const [src, setSrc] = useState(mapAnimationPath);
   
@@ -14,10 +16,22 @@ function Home(): JSX.Element {
     };
   
     return (
-      <div className="fullscreen-container">
-        <div className="background" style={{ backgroundImage: `url(${src})` }}>
-            
+      <div className="home-container">
+        <div className="background" style={{ backgroundImage: `url(${src})` }} onError={handleError}></div>
+        <div className="whitebox-container">
+            <img src={whiteBox} alt="white box"></img>
+            <div className="buttons-container">
+            <button type="button" className="btn btn-primary btn-lg">LOGIN</button>
+            <button type="button" className="btn btn-primary btn-lg">SIGNUP</button>
         </div>
+        </div>
+        <div className="logo-container">
+            <img src={groceryLogo} alt="grocery logo"></img>
+        </div>
+        {/* <div className="buttons-container">
+          <button type="button" className="btn btn-primary btn-lg" style={{ flex: '1', marginRight: '50px'}}>LOGIN</button>
+          <button type="button" className="btn btn-primary btn-lg" style={{ flex: '1'}}>SIGNUP</button>
+        </div> */}
       </div>
     );
   }
