6cd72f1f3c01607ccb0419c9649c943498cb3368
night-pianist <sweetkhiet907@gmail.com>
2024-06-28 15:21:48 -0700

    set up

--- src/App.tsx
+++ src/App.tsx
@@ -7,43 +7,44 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Signup from './components/Signup';
+import Chatbot from './components/Chatbot';
 
 
 function App() {
-  const [center, setCenter] = useState<[number, number] | null>(null);
+  // const [center, setCenter] = useState<[number, number] | null>(null);
 
-  useEffect(() => {
-    const successLocation = (position: GeolocationPosition) => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        console.log("position is " + position);
-        console.log("latitude is " + latitude + " and longitude is " + longitude);
-        setCenter([latitude, longitude]);
-    };
+  // useEffect(() => {
+  //   const successLocation = (position: GeolocationPosition) => {
+  //       const latitude = position.coords.latitude;
+  //       const longitude = position.coords.longitude;
+  //       console.log("position is " + position);
+  //       console.log("latitude is " + latitude + " and longitude is " + longitude);
+  //       setCenter([latitude, longitude]);
+  //   };
 
-    const errorLocation = () => {
-        console.error('Error getting location');
-        // Optionally set a default location if there's an error
-        setCenter([-118.4441, 34.0699]);
-    };
+  //   const errorLocation = () => {
+  //       console.error('Error getting location');
+  //       // Optionally set a default location if there's an error
+  //       setCenter([-118.4441, 34.0699]);
+  //   };
 
-    navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
-        enableHighAccuracy: true
-    });
-  }, []);
+  //   navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  //       enableHighAccuracy: true
+  //   });
+  // }, []);
 
-  if (center === null) {
-      return <div>Loading...</div>; // Or any loading indicator
-  }
+  // if (center === null) {
+  //     return <div>Loading...</div>; // Or any loading indicator
+  // }
 
   return (
       <>
         <Routes>
-          { <Route path="/" element={<Home />} /> } 
-          { <Route path="/map" element={<DestinationScreen center={center} />} /> }
+          { <Route path="/" element={<Chatbot />} /> } 
+          {/* { <Route path="/map" element={<DestinationScreen center={center} />} /> }
           { <Route path="/login" element={<Login />} /> }
           { <Route path="/signup" element={<Signup />} />}
-          { <Route path="/dashboard" element={<Dashboard />} /> }
+          { <Route path="/dashboard" element={<Dashboard />} /> } */}
         </Routes>
       </>
   )
