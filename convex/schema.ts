a6e5e8cfc8d0a8af0134da0c6df9af50906edd56
NotAmaia <012.sva@gmail.com>
2024-09-01 23:55:32 -0700

    repo not working with changed schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,14 +1,28 @@
 import { defineSchema, defineTable } from "convex/server"; 
 import { v } from "convex/values"; 
 
+// const chatMessageSchema = defineTable({
+//     msg: v.string(),
+//     type: v.string(), // Defines whether it's the chatbot or user's message
+// });
+
 export default defineSchema({
+    message: defineTable({
+        msg: v.string(),
+        userId: v.string(),
+        type: v.string(), // defines whether it's the chatbot or user's message
+    })
+    .index("byUserId",["userId"]),
+
+    // userHistory: defineTable({ 
     users: defineTable({
         userName: v.string(),
         clerkId: v.string(), // this the Clerk ID, stored in the subject JWT field
-        chatHistory: v.array(v.object({
-            type: v.string(), // defines whether it's the chatbot or user's message
-            text: v.string(),
-        })),
+        chatHistory: v.any(),
+        // chatHistory: v.array(v.object({
+        //     type: v.string(), // defines whether it's the chatbot or user's message
+        //     text: v.string(),
+        // })),
       }).index("byClerkId", ["clerkId"]),
 
 });
